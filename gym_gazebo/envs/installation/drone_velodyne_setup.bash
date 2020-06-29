8e791086163d6d323698898eb510bbeaf988ac94
TadielloM <tadistyle@gmail.com>
2020-06-29 11:35:19 +0200

    Added model and make plugin works

--- gym_gazebo/envs/installation/drone_velodyne_setup.bash
+++ gym_gazebo/envs/installation/drone_velodyne_setup.bash
@@ -12,4 +12,10 @@ else
 bash -c 'sed "s,GYM_GAZEBO_WORLD_MINE=[^;]*,'GYM_GAZEBO_WORLD_MINE=`pwd`/../assets/worlds/mine.world'," -i ~/.bashrc'
 fi
 
+if [ -z "$GAZEBO_PLUGIN_PATH" ]; then
+bash -c 'echo "export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/root/ros_ws/devel/lib/" >> ~/.bashrc'
+else
+bash -c 'sed "s,GAZEBO_PLUGIN_PATH=[^;]*,'GAZEBO_PLUGIN_PATH=/root/ros_ws/devel/lib/'," -i ~/.bashrc'
+fi
+
 exec bash # reload bash
